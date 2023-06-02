071fa07e10ed268b95c6b348ffa4882cbba68d16
Okapi911 <lucas.kloubert@gmail.com>
2023-06-02 16:53:17 +0200

    First page of the app

---
+++ ambQR/connexion.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { Appearance, useColorScheme, Button } from 'react-native';
+import { A, Nav } from '@expo/html-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {HomeScreen} from './App.js';
+
+
+export function Connexion() {
+    return (
+    <SafeAreaProvider>
+      <HomeScreen />
+    </SafeAreaProvider>
+    );
+}

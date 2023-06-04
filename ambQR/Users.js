69a0c0181ec60626d950307b039d01baa89e06d5
Jidoex <pierre.anxionnat@depinfonancy.net>
2023-06-04 21:23:29 +0200

    Changed User.js so that it can retrieve and set scores

--- ambQR/Users.js
+++ ambQR/Users.js
@@ -2,25 +2,40 @@
 
 import supabase from './helper.js'
 
-export async function getUserData(userID){
-    const { data, error } = await supabase
-    .from('Users')
-    .select('*')
-    .eq('id', userID)
-    if (error) console.log(error)
-    return data
-}
-
 export async function getCurrentUserID(){
     const { data, error } = await supabase.auth.api.getUser()
     if (error) console.log(error)
     return data.id
 }
 
-export async function getCurrentUserName(){
+export async function getCurrentScore(){
     const { data, error } = await supabase.auth.api.getUser()
     if (error) console.log(error)
     return data.user_metadata.full_name
 }
 
-//faire un Score
+export async function setCurrentScore(score){
+    const { data, error } = await supabase.auth.api.updateUser({
+        data: { full_name: score },
+    })
+    if (error) console.log(error)
+    return data
+}
+
+export async function getScoreByID(userId){
+    const { data, error } = await supabase
+    .from('Users')
+    .select('full_name')
+    .eq('id', userId)
+    if (error) console.log(error)
+    return data[0].full_name
+}
+
+export async function setUserNameByID(userId,score){
+    const { data, error } = await supabase
+    .from('Users')
+    .update({ full_name: score })
+    .eq('id', userId)
+    if (error) console.log(error)
+    return data
+}
